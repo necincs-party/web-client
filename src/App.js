b1c4add01dfd0c7f172cc1935a5285729d1cfd1b
Max Vyz <mvyznyuk@gmail.com>
2017-05-20 23:50:46 +0300

    Init the app with create-react-app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// @flow
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  div: HTMLDivElement;
+
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
